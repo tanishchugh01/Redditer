a247ca9f71be321e12894920aa59b75fe68fc1b1
tanishchugh01 <tanishchugh01@gmail.com>
2022-06-28 03:29:57 +0530

    Changed date

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -10,7 +10,7 @@ const Test = () => {
           numberOfLikes={10}
           username="tanishchugh01"
           profileName="Tanish Chugh"
-          date="Dec 10, 2021"
+          date="Jun 27, 2022"
           userProfilePicture="https://styles.redditmedia.com/t5_2qi1r/styles/communityIcon_2stg5hn8m5k51.png?width=256&s=e4abb6ac11d144c7fb965232592b4d42fe0e370b"
         />
       </div>
