92708e5a864e55d59985e8ecb86b53b25bad5c28
tanishchugh01 <tanishchugh01@gmail.com>
2022-09-03 02:32:05 +0530

    Temporarily added post in comment route

--- src/JSX/pages/Comment.jsx
+++ src/JSX/pages/Comment.jsx
@@ -1,10 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Post from "../containers/Post";
 
 const Comment = () => {
   let { id, subreddit } = useParams();
+  const [postData, setpostData] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`/r/${subreddit}/${id}.json`)
+      .then((res) => setpostData(res.data))
+  }, []);
   return (
     <div className="">
       id:{id} subreddit:{subreddit}
+      {postData ?function(){console.log(postData);return <Post post={postData[0].data.children[0]} />}() : null}
     </div>
   );
 };
